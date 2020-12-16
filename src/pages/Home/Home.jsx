6c2538d42663e517cb378f7efc569180cc91b046
Wakidur Rahman <wakidur@gmail.com>
2020-12-16 22:11:33 +0600

    result pdf

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,18 @@
+/* eslint-disable no-use-before-define */
 import React, { Component } from 'react';
+import * as XLSX from 'xlsx';
 
 export class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      csvFileHeadeFormet: false,
+      csvHeader: ['KP', 'X', 'Y', 'Z'],
       projectName: '',
       description: '',
       client: '',
       contractor: '',
-      uploadedFile: [],
       max_x: '',
       min_x: '',
       max_y: '',
@@ -19,9 +22,31 @@ export class Home extends Component {
       isStepOneShow: true,
       isStepOneDisabled: true,
       isSubmit: true,
-      fromSubmintSuccess: false,
+      formSubmitSuccess: false,
     };
   }
+
+  // Helper
+  arraysEqual = (a, b) => {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    // If you don't care about the order of the elements inside
+    // the array, you should sort both arrays here.
+    // Please note that calling sort on an array will modify that array.
+    // you might want to clone your array first.
+
+    for (let i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+  };
+
+  maxValue = (lists, prop) =>
+    [...lists].reduce((prev, curr) => (prev.prop > curr.prop ? prev : curr));
+  minValue = (lists, prop) =>
+    [...lists].reduce((prev, curr) => (prev.prop < curr.prop ? prev : curr));
 
   handleInputChange = (event) => {
     const target = event.target;
@@ -68,7 +93,7 @@ export class Home extends Component {
   };
 
   handleSubmitSuccessAlert = () => {
-    this.setState({ fromSubmintSuccess: false });
+    this.setState({ formSubmitSuccess: false });
   };
 
   handleSubmit = (event) => {
@@ -95,13 +120,103 @@ export class Home extends Component {
       isStepOneShow: true,
       isStepOneDisabled: true,
       isSubmit: true,
-      fromSubmintSuccess: true,
+      formSubmitSuccess: true,
     });
+  };
+
+  // process CSV data
+  processData = (dataString) => {
+    const dataStringLines = dataString.split(/\r\n|\n/);
+    const headers = dataStringLines[0].split(
+      /,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/
+    );
+
+    if (this.arraysEqual(headers, this.state.csvHeader)) {
+      const list = [];
+      for (let i = 1; i < dataStringLines.length; i++) {
+        const row = dataStringLines[i].split(
+          /,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/
+        );
+        if (headers && row.length == headers.length) {
+          const obj = {};
+          for (let j = 0; j < headers.length; j++) {
+            let d = row[j];
+            if (d.length > 0) {
+              if (d[0] == '"') d = d.substring(1, d.length - 1);
+              if (d[d.length - 1] == '"') d = d.substring(d.length - 2, 1);
+            }
+            if (headers[j]) {
+              obj[headers[j]] = d;
+            }
+          }
+
+          // remove the blank rows
+          if (Object.values(obj).filter((x) => x).length > 0) {
+            list.push(obj);
+          }
+        }
+      }
+      if (list && list.length > 1) {
+        const Max_X = [...list].reduce((prev, curr) =>
+          prev.X > curr.X ? prev : curr
+        );
+        const Min_X = [...list].reduce((prev, curr) =>
+          prev.X < curr.X ? prev : curr
+        );
+        const Max_Y = [...list].reduce((prev, curr) =>
+          prev.Y > curr.Y ? prev : curr
+        );
+        const Min_Y = [...list].reduce((prev, curr) =>
+          prev.Y < curr.Y ? prev : curr
+        );
+        const Max_Z = [...list].reduce((prev, curr) =>
+          prev.Z > curr.Z ? prev : curr
+        );
+        const Min_Z = [...list].reduce((prev, curr) =>
+          prev.Z < curr.Z ? prev : curr
+        );
+
+        this.setState({
+          max_x: Max_X.X,
+          min_x: Min_X.X,
+          max_y: Max_Y.Y,
+          min_y: Min_Y.Y,
+          max_z: Max_Z.Z,
+          min_z: Min_Z.Z,
+          isSubmit: false
+        });
+      } else {
+        this.setState({ csvFileHeadeFormet: false });
+      }
+      this.setState({ csvFileHeadeFormet: false });
+    } else {
+      this.setState({
+        csvFileHeadeFormet: true,
+      });
+    }
+  };
+  // handle file upload
+  handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      /* Parse data */
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, { type: 'binary' });
+      /* Get first worksheet */
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      /* Convert array of arrays */
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+      this.processData(data);
+    };
+    reader.readAsBinaryString(file);
   };
 
   render() {
     const {
-      fromSubmintSuccess,
+      csvFileHeadeFormet,
+      formSubmitSuccess,
       isStepOneShow,
       isSubmit,
       isStepOneDisabled,
@@ -109,7 +224,6 @@ export class Home extends Component {
       description,
       client,
       contractor,
-      uploadedFile,
       max_x,
       min_x,
       max_y,
@@ -120,15 +234,15 @@ export class Home extends Component {
     return (
       <div className='row'>
         <div className=' col-12 ol-sm-12 col-md-12 col-lg-12 col-xl-5 col-xxl-5'>
-          {fromSubmintSuccess ? (
+          {formSubmitSuccess ? (
             <div
-            className='alert alert-warning alert-dismissible fade show'
+              className='alert alert-warning alert-dismissible fade show'
               role='alert'
             >
               Form Data Submit Success
               <button
                 type='button'
-                class='btn-close'
+                className='btn-close'
                 data-bs-dismiss='alert'
                 aria-label='Close'
                 onClick={this.handleSubmitSuccessAlert}
@@ -213,7 +327,20 @@ export class Home extends Component {
                   <label className='input-group-text' htmlFor='csvFile'>
                     CSV File upload
                   </label>
-                  <input type='file' className='form-control' id='csvFile' />
+                  <input
+                    type='file'
+                    className={`form-control ${
+                      csvFileHeadeFormet ? 'is-invalid' : ''
+                    }`}
+                    id='csvFile'
+                    accept='.csv'
+                    onChange={this.handleFileUpload}
+                  />
+                  {csvFileHeadeFormet ? (
+                    <div className='invalid-feedback'>
+                      CSV File Header name not matching
+                    </div>
+                  ) : null}
                 </div>
                 {/* Max_X */}
                 <div className='form-floating mb-3'>
--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 export class Result extends Component {
   constructor(props) {
@@ -11,9 +14,37 @@ export class Result extends Component {
 
   componentDidMount() {
     const projectList = JSON.parse(localStorage.getItem('ABCEngine'));
-    console.log(projectList);
     this.setState({ projectList });
   }
+
+  downloadPDF = () => {
+    const { projectList } = this.state;
+    const docDefinition = {
+      content: [
+        {
+          layout: 'lightHorizontalLines', // optional
+          table: {
+            // headers are automatically repeated if the table spans over multiple pages
+            // you can declare how many rows should be treated as headers
+            headerRows: 1,
+            widths: [100, 150, 150, 150, '*'],
+
+            body: [
+              ['S.No', 'Name', 'Desc', 'Client', 'Contractor'],
+              ...projectList.map((o, i) => [
+                i + 1,
+                o.projectName,
+                o.description,
+                o.client,
+                o.contractor,
+              ]),
+            ],
+          },
+        },
+      ],
+    };
+    pdfMake.createPdf(docDefinition).download('result.pdf');
+  };
 
   render() {
     const { projectList } = this.state;
@@ -22,7 +53,11 @@ export class Result extends Component {
         <div className=' d-flex justify-content-between mt-4 mb-5'>
           <h3>Project List </h3>
           {projectList && projectList.length > 0 ? (
-            <button type='button' className='btn btn-outline-success'>
+            <button
+              type='button'
+              onClick={this.downloadPDF}
+              className='btn btn-outline-success'
+            >
               Download
             </button>
           ) : null}
